6a2e859da13171164fe17820573b20042da15490
Daniel Krivelev <krivelevdaniil@gmail.com>
2022-01-16 19:56:35 +0700

    Add files via upload

---
+++ CustomTemplate/WorldSkills.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,29 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+class ___FILEBASENAMEASIDENTIFIER___Model: ObservableObject {
+  @Published var name = "Daniel"
+}
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+  var isNavigationBarHidden = true
+  
+  @StateObject var viewModel = ___FILEBASENAMEASIDENTIFIER___Model()
+  
+  var body: some View {
+    NavigationView {
+      GeometryReader { screen in
+        Text("Hello, \(viewModel.name)!")
+      }
+    }
+    .navigationBarHidden(isNavigationBarHidden)
+  }
+}
+
+struct ___FILEBASENAMEASIDENTIFIER____Previews: PreviewProvider {
+  static var previews: some View {
+    ___FILEBASENAMEASIDENTIFIER___()
+  }
+}
+
